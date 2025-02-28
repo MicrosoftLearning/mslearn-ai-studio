81d0a286d7952b4cc8089a9c7d10d77793dee869
Graeme Malcolm <gmalc@microsoft.com>
2025-02-28 11:08:02 -0800

    Added RAG app

---
+++ labfiles/rag-app/c-sharp/Program.cs
@@ -0,0 +1,99 @@
+﻿/* Required packages
+dotnet add package Azure.AI.Projects --prerelease
+dotnet add package Azure.Identity
+dotnet add package Azure.AI.OpenAI --prerelease */
+
+using System;
+using Azure;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Azure.Identity;
+using Azure.AI.Projects;
+using Azure.AI.OpenAI;
+using System.ClientModel;
+using Azure.AI.OpenAI.Chat;
+using OpenAI.Chat;
+
+
+namespace rag_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Get config settings
+                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                IConfigurationRoot configuration = builder.Build();
+                string project_connection = configuration["PROJECT_CONNECTION"];
+                string model_deployment = configuration["MODEL_DEPLOYMENT"];
+                string index_name = configuration["INDEX_NAME"];
+
+                // Initialize the project client
+                var projectClient = new AIProjectClient(project_connection,
+                    new DefaultAzureCredential());
+                
+                var connectionsClient = projectClient.GetConnectionsClient();
+
+                // Use the AI search service connection to get service details
+                ConnectionResponse searchConnection = connectionsClient.GetDefaultConnection(ConnectionType.AzureAISearch, true);
+                var searchProperties = searchConnection.Properties as ConnectionPropertiesApiKeyAuth;
+                var credential = new AzureKeyCredential(searchProperties.Credentials.Key);
+                Uri endpoint = new Uri(searchProperties.Target);
+                string search_url = searchProperties.Target;
+                string search_key = searchProperties.Credentials.Key;
+
+                // Get an Azure OpenAI client
+                ConnectionResponse aoaiConnection = connectionsClient.GetDefaultConnection(ConnectionType.AzureAIServices, true);
+                var aoaiProperties = aoaiConnection.Properties as ConnectionPropertiesApiKeyAuth;
+                AzureOpenAIClient azureOpenAIClient = new(
+                    new Uri(aoaiProperties.Target),
+                    new AzureKeyCredential(aoaiProperties.Credentials.Key));
+
+                // Loop until the user types 'quit'
+                string input_text = "";
+                while (input_text.ToLower() != "quit")
+                {
+                    // Get user input
+                    Console.WriteLine("Enter the prompt (or type 'quit' to exit):");
+                    input_text = Console.ReadLine();
+                    if (input_text.ToLower() != "quit")
+                    {
+                        // Get a chat completion
+                        ChatClient chatClient = azureOpenAIClient.GetChatClient(model_deployment);
+
+                        // dditional parameters to apply RAG pattern using the AI Search index
+                        // (DataSource is in preview and subject to breaking changes)
+                        #pragma warning disable AOAI001
+
+                        ChatCompletionOptions options = new();
+                        options.AddDataSource(new AzureSearchChatDataSource()
+                        {
+                            Endpoint = new Uri(search_url),
+                            IndexName = index_name,
+                            Authentication = DataSourceAuthentication.FromApiKey(search_key),
+                        });
+
+                        ChatCompletion completion = chatClient.CompleteChat(
+                            [
+                                new SystemChatMessage("You are an AI travel assistant that helps people plan trips."),
+                                new UserChatMessage(input_text),
+                            ],
+                            options);
+
+                        Console.WriteLine($"{completion.Role}: {completion.Content[0].Text}");
+                        
+                        #pragma warning restore AOAI001
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
---
+++ labfiles/rag-app/python/rag-app.py
@@ -0,0 +1,81 @@
+"""
+Required Libraries
+pip install python-dotenv azure-ai-projects azure-identity openai
+"""
+
+import os
+from dotenv import load_dotenv
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+from azure.ai.projects.models import ConnectionType
+import openai
+
+def main(): 
+        
+    try: 
+    
+        # Get configuration settings 
+        load_dotenv()
+        project_connection = os.getenv("PROJECT_CONNECTION")
+        model_deployment =  os.getenv("MODEL_DEPLOYMENT")
+        index_name = os.getenv("INDEX_NAME")
+        
+        # Initialize the project client
+        projectClient = AIProjectClient.from_connection_string(
+            conn_str=project_connection,
+            credential=DefaultAzureCredential())
+
+        # Use the AI search service connection to get service details
+        searchConnection = projectClient.connections.get_default(
+            connection_type=ConnectionType.AZURE_AI_SEARCH,
+            include_credentials=True,
+        )
+        search_url = searchConnection.endpoint_url
+        search_key = searchConnection.key
+
+        ## Get an OpenAI chat client
+        aoai_client = projectClient.inference.get_azure_openai_client(api_version="2024-10-21")
+
+        # Loop until the user types 'quit'
+        while True:
+            # Get input text
+            input_text = input("Enter the prompt (or type 'quit' to exit): ")
+            if input_text.lower() == "quit":
+                break
+            if len(input_text) == 0:
+                print("Please enter a prompt.")
+                continue
+            
+            # Get a chat completion
+            completion = aoai_client.chat.completions.create(
+                model=model_deployment,
+                messages=[
+                    {"role": "system", "content": "You are an AI travel assistant that helps people plan trips."},
+                    {"role": "user", "content": input_text},
+                ],
+                ## additional parameters to apply RAG pattern using the AI Search index
+                extra_body={
+                    "data_sources":[
+                        {
+                            "type": "azure_search",
+                            "parameters": {
+                                "endpoint": search_url,
+                                "index_name": index_name,
+                                "authentication": {
+                                    "type": "api_key",
+                                    "key": search_key,
+                                }
+                            }
+                        }
+                    ],
+                }
+            )
+
+            print(completion.choices[0].message.content)
+
+
+    except Exception as ex:
+        print(ex)
+
+if __name__ == '__main__': 
+    main()
